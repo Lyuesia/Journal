bbd80676db43803dd38a37065c8b0b63c2812040
Bum JunKwon <lyuesia@gmail.com>
2018-07-21 15:35:03 +0900

    폴더 구조 수정

--- Journal/Sources/Controllers/ViewController.swift
+++ Journal/Sources/Controllers/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+//    logo가 뜬 후 컨트롤이 가능한 view가 불러와지는점시점
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
