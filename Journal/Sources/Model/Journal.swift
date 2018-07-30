71c968bffa618694e1cb1f44c0211c04ef809ece
Bum JunKwon <lyuesia@gmail.com>
2018-07-30 19:09:13 +0900

    protocol 추가, class name, func name 수정

--- Journal/Sources/Model/Journal.swift
+++ Journal/Sources/Model/Journal.swift
@@ -2,31 +2,15 @@
 //  Journal.swift
 //  Journal
 //
-//  Created by Bum JunKwon on 28/07/2018.
+//  Created by Bum JunKwon on 30/07/2018.
 //  Copyright © 2018 Bum Jun Kwon. All rights reserved.
 //
 
 import Foundation
 
-class Journal {
-    
-    var book = [Entry]()
-    
-    func createEntry(with content: String) {
-        let newEntry = Entry(date: Date(), content: content)
-        book.append(newEntry)
-    }
-    
-    func editEntry(of index: Int, to content: String) {
-        //code
-        reorganizeBook()
-    }
-    
-    func deleteEntry(of index: Int) {
-        //code
-    }
-    
-    func reorganizeBook() {
-        
-    }
+protocol Journal {
+    func add(_ entry: Entry)
+    func update(_ entry: Entry)
+    func remove(_ entry: Entry)
+    func recentEntries(max: Int) -> [Entry]
 }
