d96d52eb5cade9412be75e9c0dee9990adc9fd33
Bum JunKwon <lyuesia@gmail.com>
2018-08-04 16:41:24 +0900

    snapkit 추가

--- Journal/Sources/Controllers/ViewController.swift
+++ Journal/Sources/Controllers/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
@@ -15,9 +16,10 @@ class ViewController: UIViewController {
 //    logo가 뜬 후 컨트롤이 가능한 view가 불러와지는점시점
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
